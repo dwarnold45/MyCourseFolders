b72cba1a225f25389551a4c7a0dc88964dcd73fc
David Arnold <dwarnold45@suddenlink.net>
2014-05-10 12:11:33 -0700

    First commit
    Good luck

--- ExploratoryDataAnalysis/Project1/plot1.R
+++ ExploratoryDataAnalysis/Project1/plot1.R
@@ -1,8 +1,13 @@
+# Here I followed a suggestion on the suggestion forum which led me to some
+# learging of the sqldf package.
+
 library(sqldf)
 myfile <- "household_power_consumption.txt"
 mySql <- "SELECT * from file WHERE Date='1/2/2007' OR Date='2/2/2007'"
 myData <- read.csv.sql(myfile,sql=mySql,sep=";")
 str(myData)
+
+png(file="plot1.png", width=480, height=480)
 
 hist(myData$Global_active_power,
      xlab="Global Active Power (kilowatts)",
@@ -10,3 +15,4 @@ hist(myData$Global_active_power,
      main="Global Active Power",
      col="red")
 
+dev.off()
